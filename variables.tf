65fde0965fb3d67a44409384058dbe5b497dfe66
theppasiva <theppasiva@gmail.com>
2025-03-26 01:53:28 +0530

    expense infra

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "vpc_cidr"{
 
 }
 
-variable "enable_dns_hostname"{
+variable "enable_dns_hostnames"{
     default = true
 }
 
@@ -23,4 +23,64 @@ variable "common_tags"{
 
 variable "vpc_tags"{
     default = {}
+}
+variable "igw_tags"{
+    default ={}
+}
+
+variable "public_subnet_cidrs"{
+    type =list
+    validation {
+    condition     = length(var.public_subnet_cidrs) == 2
+    error_message = "please provide 2 valid public_subnet_cidrs"
+  }
+}
+variable "public_subnet_tags"{
+    default = {}
+}
+
+variable "private_subnet_cidrs"{
+    type =list
+    validation {
+    condition     = length(var.private_subnet_cidrs) == 2
+    error_message = "please provide 2 valid private_subnet_cidrs"
+  }
+}
+variable "private_subnet_tags"{
+    default = {}
+}
+
+variable "database_subnet_cidrs"{
+    type =list
+    validation {
+    condition     = length(var.database_subnet_cidrs) == 2
+    error_message = "please provide 2 valid database_subnet_cidrs"
+  }
+}
+variable "database_subnet_tags"{
+    default = {}
+}
+
+variable "nat_gateway_tags"{
+    default = {}
+}
+
+variable "public_route_table_tags"{
+    default = {}
+}
+
+variable "private_route_table_tags"{
+    default = {}
+}
+
+variable "database_route_table_tags"{
+    default = {}
+}
+
+variable "is_peering_required"{
+    default = false
+}
+
+variable "vpc_peering_tags"{
+default = {}
 }
